749592099f98acea5ddd9caf0c3b273374fd2753
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-15 20:43:08 +0300

    remove irrelevant data

--- routes/index.js
+++ routes/index.js
@@ -48,32 +48,4 @@ router.post("/upload", async function (req, res) {
     res.redirect("/");
 });
 
-// router.use("/form", express.static(__dirname + "/index.html"));
-
-// router.post("/upload", function (req, res) {
-//     let sampleFile;
-//     let uploadPath;
-
-//     if (!req.files || Object.keys(req.files).length === 0) {
-//         res.status(400).send("No files were uploaded.");
-//         return;
-//     }
-
-//     console.log("req.files >>>", req.files); // eslint-disable-line
-
-//     sampleFile = req.files.sampleFile;
-
-//     uploadPath = __dirname + "/uploads/" + sampleFile.name;
-
-//     sampleFile.mv(uploadPath, function (err) {
-//         if (err) {
-//             return res.status(500).send(err);
-//         }
-
-//         res.send("File uploaded to " + uploadPath);
-//     });
-// });
-
-// router.use("/form", express.static(__dirname + "/index.html"));
-
 module.exports = router;
