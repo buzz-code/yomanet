7a36ee2a11bc41f231255bff9ee3e14485a03fd1
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-20 01:58:09 +0300

    add report to every table

--- controllers/data.js
+++ controllers/data.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
-const { getTableDataResponse } = require("../helpers/normalizer");
+const { getTableDataResponse, createReport } = require("../helpers/normalizer");
 
 function registerHook(hook) {
     router.post(hook.url, auth, async function (req, res) {
-        const query = await hook.query(req.body, req.user);
-        console.log(req.body, query);
+        const filter = JSON.parse(req.body.filter);
+        const query = await hook.query(filter, req.user);
+        console.log(filter, query);
 
         const isValid = await hook.validate(query, req.user);
         if (!isValid) {
@@ -14,11 +15,29 @@ function registerHook(hook) {
             return;
         }
 
-        const results = await hook.data(query, req.body.page);
+        const results = await hook.data(query, filter.page);
         const count = await hook.count(query);
-        const headers = await hook.headers(results, query, req.body);
+        const headers = await hook.headers(results, query, filter);
 
-        getTableDataResponse(res, results, count, headers, req.body);
+        getTableDataResponse(res, results, count, headers, filter);
+    });
+    router.get(hook.url, auth, async function (req, res) {
+        const filter = JSON.parse(req.query.filter);
+        const query = await hook.query(filter, req.user);
+        console.log(filter, query);
+
+        const isValid = await hook.validate(query, req.user);
+        if (!isValid) {
+            res.send({ error: true });
+            return;
+        }
+
+        const results = await hook.data(query, -1);
+        const headers = await hook.headers(results, query, filter);
+        //todo: make title more correct
+        const title = hook.title();
+
+        return createReport(res, title, results, headers);
     });
 }
 
