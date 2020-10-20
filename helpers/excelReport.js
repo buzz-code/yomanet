f89a7893292d29fe46dbcb958cbc4bcc0e6d2623
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-20 10:30:53 +0300

    excel add styling to header

--- helpers/excelReport.js
+++ helpers/excelReport.js
@@ -8,12 +8,22 @@ const createExcelReport = async (res, title, results, headers) => {
         "Transfer-Encoding": "chunked",
         "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     });
-    var workbook = new ExcelJS.stream.xlsx.WorkbookWriter({ stream: res });
+    var workbook = new ExcelJS.stream.xlsx.WorkbookWriter({ stream: res, useStyles: true });
     var worksheet = workbook.addWorksheet(title);
-    worksheet.addRow(headers.map((item) => item.label));
-    //todo: check why not working
-    worksheet.getRow(1).font = { color: { argb: "563d7c00" } };
-    worksheet.getRow(1).commit();
+    const header = worksheet.addRow(headers.map((item) => item.label));
+    header.eachCell((cell) => {
+        cell.fill = {
+            type: "pattern",
+            pattern: "solid",
+            fgColor: { argb: "563d7c" },
+        };
+        cell.font = {
+            size: 12,
+            bold: true,
+            color: { argb: "ffffff" },
+        };
+    });
+    header.commit();
     results.forEach((item) => {
         const row = headers.map((header) => getTableCellValue(item, header));
         worksheet.addRow(row).commit();
