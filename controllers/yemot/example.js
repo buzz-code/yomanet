ab203ea801c3622dd07713e6d2cde7a4470f043e
buzz-code <hadasa.schechter@gmail.com>
2020-10-25 22:22:02 +0000

    connect to yemot api

---
+++ controllers/yemot/example.js
@@ -0,0 +1,11 @@
+const yemotApi = require('./index')
+
+async function main() {
+    const yemot = new yemotApi("033069265", "7525");
+    const { data } = await yemot.exec("GetIVR2Dir", { path: "ivr2:Log/LogPlaybackPlayStop" });
+    console.log(data.html.map(item => ({ label: item.name, value: item.what })));
+    // const file = await yemot.exec("DownloadFile", { path: files.data.html[0].what });
+    // return file.data;
+}
+
+main().then(console.log)
