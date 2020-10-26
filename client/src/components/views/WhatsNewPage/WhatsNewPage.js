8807c43f5623256541916cf42f91b3da82d94dfa
buzz-code <hadasa.schechter@gmail.com>
2020-10-26 07:16:27 +0000

    update whats-new page

--- client/src/components/views/WhatsNewPage/WhatsNewPage.js
+++ client/src/components/views/WhatsNewPage/WhatsNewPage.js
@@ -5,6 +5,10 @@ function WhatsNewPage() {
         <div className="app">
             <article className="container whats-new">
                 <h2 className="pt-3">אפשרויות שנוספו במערכת</h2>
+                <h3>ז' חשוון תשפ"א</h3>
+                <ul>
+                    <li>נוספו דוחות האזנה לפי מגמה</li>
+                </ul>
                 <h3>ו' חשוון תשפ"א</h3>
                 <ul>
                     <li>נוספה תמיכה בסינון לפי מגמה</li>
