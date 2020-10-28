2a180cab434645e594c5434e9c599f5c9dae7937
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-28 02:14:49 +0200

    make loader overlay

--- client/src/components/widgets/Loader.js
+++ client/src/components/widgets/Loader.js
@@ -6,10 +6,15 @@ export default function Loader() {
         <div
             style={{
                 width: "100%",
-                height: "100",
+                height: "100%",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
+                position: "fixed",
+                top: "0",
+                right: "0",
+                backgroundColor: "#33333333",
+                zIndex: "999999",
             }}>
             <CustomLoader type="ThreeDots" color="#563d7c" height="100" width="100" />
         </div>
