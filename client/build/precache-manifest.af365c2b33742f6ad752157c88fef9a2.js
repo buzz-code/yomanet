ed3b5fea722ec55d5b61c6c9df626f71f1414662
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-29 22:03:53 +0200

    update build folder

---
+++ client/build/precache-manifest.af365c2b33742f6ad752157c88fef9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc432ac4836f5c6c8d3f38f131671ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b058cd57d16f45d780ab",
+    "url": "/static/css/2.dd57636e.chunk.css"
+  },
+  {
+    "revision": "9eaee5563979dc3567b7",
+    "url": "/static/css/main.ae4907d9.chunk.css"
+  },
+  {
+    "revision": "b058cd57d16f45d780ab",
+    "url": "/static/js/2.9222196e.chunk.js"
+  },
+  {
+    "revision": "595f8b534307677d008bb80a16b7267d",
+    "url": "/static/js/2.9222196e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eaee5563979dc3567b7",
+    "url": "/static/js/main.c799f0cb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
