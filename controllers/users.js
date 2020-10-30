0dcb74accdda4ad73d1ebbd4c23d887d18cc5a7c
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-10-30 14:18:15 +0200

    add console.logs

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { User } = require("../models/User");
 
@@ -22,38 +22,35 @@ router.get("/auth", auth, (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-
     const user = new User(req.body);
 
     user.save((err, doc) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).json({
-            success: true
+            success: true,
         });
     });
 });
 
 router.post("/login", (req, res) => {
+    console.log("login start");
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user)
             return res.json({
                 loginSuccess: false,
-                message: "Auth failed, email not found"
+                message: "Auth failed, email not found",
             });
-
+        console.log("found user", user);
         user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
+            if (!isMatch) return res.json({ loginSuccess: false, message: "Wrong password" });
 
             user.generateToken((err, user) => {
                 if (err) return res.status(400).send(err);
                 res.cookie("w_authExp", user.tokenExp);
-                res
-                    .cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
+                res.cookie("w_auth", user.token).status(200).json({
+                    loginSuccess: true,
+                    userId: user._id,
+                });
             });
         });
     });
@@ -63,7 +60,7 @@ router.get("/logout", auth, (req, res) => {
     User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
         if (err) return res.json({ success: false, err });
         return res.status(200).send({
-            success: true
+            success: true,
         });
     });
 });
