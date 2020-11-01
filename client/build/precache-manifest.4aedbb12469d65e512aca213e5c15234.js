40ff1a2d5ad26db7ce4c9c3649aa49dc3f84a847
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-11-01 21:45:51 +0200

    update client

--- client/build/precache-manifest.4aedbb12469d65e512aca213e5c15234.js
+++ client/build/precache-manifest.4aedbb12469d65e512aca213e5c15234.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "893bc94ade2baf57867829795a502e19",
+    "revision": "63d55621383d9ec2f39cdbbb49bd6faf",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.dd57636e.chunk.css"
   },
   {
-    "revision": "a26e67266259fd8c6744",
+    "revision": "84fe52c92d383b69fe88",
     "url": "/static/css/main.ae4907d9.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.5408ab7b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a26e67266259fd8c6744",
-    "url": "/static/js/main.f1a2abb0.chunk.js"
+    "revision": "84fe52c92d383b69fe88",
+    "url": "/static/js/main.e2bd09fc.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
