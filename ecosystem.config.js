0a0c4a8b1ccebc0f38a6bc48b8ae7b3ba56d3857
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-11-02 00:41:58 +0200

    use multiple clusters in pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,8 @@ module.exports = {
                 NODE_ENV: "production",
                 MONGO_URI: "mongodb://127.0.0.1:27018/vocal?retryWrites=false",
             },
+            instances: "max",
+            exec_mode: "cluster",
         },
     ],
 };
