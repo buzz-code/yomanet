ac4ebe4328c627f75c544f207bb9a17828aca25b
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-11-02 22:24:47 +0200

    update mongoURI

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 module.exports = {
-    mongoURI:`mongodb+srv://user:LTfJoXV9VX7JA7KX@vocal-center-stats.ymyj5.gcp.mongodb.net/vocal?retryWrites=true&w=majority`
+    mongoURI: `mongodb://myUserAdmin:1@145.239.83.4:27018/vocal?authSource=admin`,
+    // mongoURI:`mongodb+srv://user:LTfJoXV9VX7JA7KX@vocal-center-stats.ymyj5.gcp.mongodb.net/vocal?retryWrites=true&w=majority`
     // mongoURI:`mongodb://myUserAdmin:1@138.201.6.204:27017/admin`
-}
+};
