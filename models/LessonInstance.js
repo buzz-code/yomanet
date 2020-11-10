d5b59b5f21733be2be8d840dcf26063ae81afa00
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-11-11 01:08:23 +0200

    add lesson instance class

---
+++ models/LessonInstance.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const lessonInstanceSchema = mongoose.Schema({
+    user: { type: String, trim: true },
+    createdAt: { type: Date, default: Date.now },
+    Folder: { type: String, trim: true },
+    Current: { type: String, trim: true },
+    FileLength: { type: Number },
+});
+
+lessonInstanceSchema.index({ user: 1 });
+lessonInstanceSchema.index({ user: 1, Folder: 1 });
+lessonInstanceSchema.index({ user: 1, Current: 1 });
+lessonInstanceSchema.index({ user: 1, Folder: 1, Current: 1 });
+
+const LessonInstance = mongoose.model("LessonInstance", lessonInstanceSchema);
+
+module.exports = { LessonInstance };
---
+++ scripts/updateLessonInstaces.js
@@ -0,0 +1,57 @@
+const { User } = require("../models/User");
+const mongoose = require("mongoose");
+const { mongoURI } = require("./../config/dev");
+const { YemotPlayback } = require("../models/YemotPlayback");
+const { LessonInstance } = require("../models/LessonInstance");
+
+async function main() {
+    console.log("updateLessonInstance start");
+    try {
+        const connect = mongoose
+            .connect(mongoURI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+            })
+            .then(() => console.log("MongoDB Connected..."))
+            .catch((err) => console.log("MongoDB connect error", err));
+
+        const users = await User.find({ provider: "yemot" }).lean();
+        for (const user of users) {
+            console.log("start process for user:", user.name);
+            try {
+                const data = await YemotPlayback.aggregate()
+                    .match({ user: user.name, FileLength: { $ne: null } })
+                    .group({
+                        _id: { Folder: "$Folder", Current: "$Current" },
+                        FileLength: { $first: "$FileLength" },
+                    })
+                    .project({
+                        _id: 0,
+                        user: user.name,
+                        Folder: "$_id.Folder",
+                        Current: "$_id.Current",
+                        FileLength: "$FileLength",
+                    });
+
+                await LessonInstance.deleteMany({ user: user.name });
+                await LessonInstance.insertMany(data);
+
+                console.log("end process for user:", user.name);
+            } catch (err) {
+                console.log("error for user:", user.name, err);
+            }
+        }
+        console.log("updateLessonInstance finished successfully");
+    } catch (err) {
+        console.log("updateLessonInstance finished with an error", err);
+    } finally {
+        mongoose.connection.close(function () {
+            console.log("Mongoose connection disconnected");
+            process.exit(0);
+        });
+    }
+}
+
+main().then(console.log);
