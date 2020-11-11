60853d692306bd96d16ac6273b2d7f47e047db6d
buzz-code <72219643+buzz-code@users.noreply.github.com>
2020-11-11 21:48:36 +0200

    add dataByMegama template

--- controllers/data/listeningByMegama.js
+++ controllers/data/listeningByMegama.js
@@ -1,44 +1,4 @@
 const { YemotPlayback } = require("../../models/YemotPlayback");
-const { Student } = require("../../models/Student");
-const titleUtil = require("../../helpers/dataUtils/titleUtil");
-const queryUtil = require("../../helpers/dataUtils/queryUtil");
-const { getExtensionHeaders, getDataById } = require("../../helpers/dataUtils/utils");
-const { getAggregateByKlassOrMegama } = require("../../helpers/dataUtils/aggregateUtil");
+const dataByMegama = require("./templates/dataByMegama");
 
-module.exports = {
-    url: "/listeningByMegama",
-    title: function (filter) {
-        return titleUtil.getTitle("דוח האזנה", filter, titleUtil.singleMegama, titleUtil.lesson, titleUtil.dates);
-    },
-    query: async function (filter, user) {
-        const query = queryUtil.getQuery(user, filter, queryUtil.dates, queryUtil.lesson);
-        const studentQuery = queryUtil.getQuery(user, filter, queryUtil.megama);
-
-        return { query, studentQuery };
-    },
-    validate: async function (query, user, filter) {
-        if (filter.megama && filter.megama.length) {
-            return { isValid: true, errorMessage: null };
-        }
-        return { isValid: false, errorMessage: "חובה לבחור מגמה" };
-    },
-    data: async function (queries, page) {
-        const { query, students } = await queryUtil.getQueryWithStudentIds(queries, page);
-
-        const dataById = await getDataById(YemotPlayback, getAggregateByKlassOrMegama(query));
-
-        return students.map((item) => ({
-            name: item.name,
-            ...dataById[item.identityNumber],
-        }));
-    },
-    headers: async function (data, query, filter, user) {
-        const headers = [{ label: "שם התלמידה", value: "name" }, ...getExtensionHeaders(user, data)];
-
-        return headers;
-    },
-    count: async function (queries) {
-        const { studentQuery } = queries;
-        return await Student.countDocuments({ $and: studentQuery });
-    },
-};
+module.exports = dataByMegama(YemotPlayback, "/listeningByMegama", "דוח האזנה");
