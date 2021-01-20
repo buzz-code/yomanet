e763e555da2411c17c786e724e9eaa111da82bf5
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-01-20 09:54:46 +0200

    remove notice of beta version

--- client/src/components/widgets/Footer/Footer.js
+++ client/src/components/widgets/Footer/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
             <div className="container d-flex">
                 <span className="text-muted">למידע ופרטים נוספים 050-4105475 או hadasa.schechter@gmail.com</span>
                 <div className="flex-fill"></div>
-                <span className="ml-auto small">האתר בהרצה, נשמח אם תעדכנו אותנו על כל תקלה</span>
+                {/* <span className="ml-auto small">האתר בהרצה, נשמח אם תעדכנו אותנו על כל תקלה</span> */}
             </div>
         </footer>
     );
