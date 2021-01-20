c14e74fea8003b9a1e3f6bb8c6c63edb4e1534a5
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-01-20 09:57:28 +0200

    update dev connction string

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
     // mongoURI: `mongodb://myUserAdmin:1@145.239.83.4:27018/vocal?authSource=admin`,
     // mongoURI:`mongodb+srv://user:LTfJoXV9VX7JA7KX@vocal-center-stats.ymyj5.gcp.mongodb.net/vocal?retryWrites=true&w=majority`
     // mongoURI:`mongodb://myUserAdmin:1@138.201.6.204:27017/admin`
-    mongoURI: "mongodb://localhost:27017/vocal",
-    // mongoURI: "mongodb://USERNAME:PASSWORD@localhost:PORT/vocal?authSource=admin",
+    // mongoURI: "mongodb://localhost:27017/vocal",
+    mongoURI: "mongodb://USERNAME:PASSWORD@localhost:PORT/vocal?authSource=admin",
 };
