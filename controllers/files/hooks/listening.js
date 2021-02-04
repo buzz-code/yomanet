3941bd35c150d0b498e61b81ce4e6a60d42d3153
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-02-04 17:14:01 +0200

    update regex for nevat

--- controllers/files/hooks/listening.js
+++ controllers/files/hooks/listening.js
@@ -3,6 +3,6 @@ module.exports = {
     yemotPath: "ivr2:Log/LogPlaybackPlayStop",
     yedaUrl: "https://yeda-phone.com/bsh/api/sfu-",
     dirRegex: /^nothing$/,
-    fileRegex: /^((LogPlaybackPlayStop\.[\d]{4,4}-[\d][\d]-[\d][\d]\.ymgr)|([a-z]*\.[\d]*\.csv))|([א-ת\s]*[\d]{4,4}-[\d][\d]-[\d][\d].csv)$/,
+    fileRegex: /^((LogPlaybackPlayStop\.[\d]{4,4}-[\d][\d]-[\d][\d]\.ymgr)|([a-z]*\.[\d]*\.csv))|([א-ת\s]*[\d]{4,4}-[\d][\d]-[\d][\d].csv)|(דוח שיעורים נוות ישראל היסטוריה.csv)$/,
     fileType: "LogPlaybackPlayStop",
 };
